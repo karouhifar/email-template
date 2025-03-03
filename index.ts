9d9484b739bb036909590aa6a31d24aa27cb3577
karouhifar <krouhifar@myseneca.ca>
2025-03-03 01:37:27 -0500

    dockerize the conainer bun

--- index.ts
+++ index.ts
@@ -1,10 +1,24 @@
-
-
+const PORT = parseInt(process.env.PORT || "3000");
 
 const server = Bun.serve({
-  port: 3000,
-  fetch(req) {
-    return new Response("Bun!");
+  port: PORT,
+  async fetch(req) {
+    const { url, method } = req;
+    const path = new URL(url).pathname;
+
+    if (path === "/" && method === "GET") {
+      return new Response("Hello, World!", {headers: {"Content-Type": "text/plain"}});
+    }
+    else if (path === "/send-email" && method === "POST") {
+        try {
+            const data = await req.json();
+            return new Response(JSON.stringify(data), {headers: {"Content-Type": "application/json"}});
+        } catch (error : any) {
+            return new Response(JSON.stringify({error: "Invalid response"}), {status: 400});
+        }  
+    }
+
+    return new Response("Not found!",{status: 404});
   },
 });
 
