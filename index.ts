e6689be72df432ed0e740fba66ddb595e8f8ff3b
karouhifar <krouhifar@myseneca.ca>
2025-03-09 15:17:08 -0400

    updated changes

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
+import sgMail from "@sendgrid/mail";
+
+
+
+
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 Bun.serve({
@@ -12,14 +17,38 @@ Bun.serve({
       });
     } else if (path === "/send-email" && method === "POST") {
       try {
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
         const data = await req.json();
-        return new Response(JSON.stringify(data), {
-          headers: { "Content-Type": "application/json" },
+        const msg = {
+          to: data.recipient, // Change to your recipient
+          from: "Ritz Shrivastav <hritika12245@gmail.com>", // Change to your verified sender
+          subject: "Message Received – Thank You!",
+          templateId: "d-7122b4b005f648798de3822bed90214a",
+          dynamic_template_data: {
+            name: data?.name ?? data.recipient.split("@")[0],
+          },
+        };
+        const msg2 = {
+          to: "hritika12245@gmail.com", // Change to your recipient
+          from: "Ritz Shrivastav <hritika12245@gmail.com>", // Change to your verified sender
+          subject:
+            "Message from " + (data?.name ? ( data?.name + "<" + data.recipient + ">") : data.recipient),
+          text: data.message,
+          html: `<strong>${data.message} - LinkedIn : ${data?.LinkedIn ?? "Not Provided"}</strong>`,
+        };
+       const res = await sgMail.send(msg);
+       const received = await sgMail.send(msg2);
+       if (!res || !received) throw new Error("Invalid response"); 
+        return new Response("Email sent!", {
+          headers: { "Content-Type": "text/plain" },
         });
       } catch (error: any) {
-        return new Response(JSON.stringify({ error: "Invalid response" }), {
-          status: 400,
-        });
+        return new Response(
+          JSON.stringify({ error: "Invalid response : " + error?.message }),
+          {
+            status: 400,
+          }
+        );
       }
     }
 
